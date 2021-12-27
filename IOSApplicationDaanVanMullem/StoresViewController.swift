c1cf759ad2fcdd2d0536fea2c9b6b3fc0a9cffce
Daan <daan.vanmullem@student.hogent.be>
2021-12-27 20:41:07 +0000

    Added StoresViewController with tableview and API access

---
+++ IOSApplicationDaanVanMullem/StoresViewController.swift
@@ -0,0 +1,79 @@
+//
+//  StoresViewController.swift
+//  FirstProjectTest2
+//
+//  Created by user208206 on 12/27/21.
+//
+
+import UIKit
+
+class StoresViewController: UITableViewController  {
+
+    var game = Game(currency: "", currentLowestPrice: 0, developer: "", id: "", name: "", releaseDate: "", stores: [], type: "")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let headers = [
+            "x-rapidapi-host": "game-prices.p.rapidapi.com",
+            "x-rapidapi-key": "c513e51ec6msh882eb3b4c9d62c4p17cedcjsndf0ee44625fc"
+        ]
+
+        let request = NSMutableURLRequest(url: NSURL(string: "https://game-prices.p.rapidapi.com/game/minecraft?region=us&type=game")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest) {
+            (data, response, error) in
+            guard let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200 else {
+                return
+            }
+            
+            guard let data = data else {
+                print(error.debugDescription)
+                return
+            }
+            
+            if let result = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as String? {
+                print(result)
+            }
+            
+            self.parse(json: data)
+            
+        }
+        dataTask.resume()
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        if let jsonGames = try? decoder.decode(Game.self, from: json){
+            game = Game(currency: jsonGames.currency, currentLowestPrice: jsonGames.currentLowestPrice, developer: jsonGames.developer, id: jsonGames.id, name: jsonGames.name, releaseDate: jsonGames.releaseDate, stores: jsonGames.stores, type: jsonGames.type)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return game.stores.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)  -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let store = game.stores[indexPath.row]
+        cell.textLabel?.text = store.seller
+        cell.detailTextLabel?.text = String(format: "%f", store.price)
+        cell.imageView?.image =  UIImage(named: store.seller)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let store = game.stores[indexPath.row]
+        if let url = URL(string: store.url){
+            UIApplication.shared.open(url)
+        }
+    }
+}
