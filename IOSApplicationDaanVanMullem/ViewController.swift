2a9b0b2609074ad480a6d3426275189cdd0fa3df
Daan <daan.vanmullem@student.hogent.be>
2021-12-28 19:26:54 +0000

    Added PickerView for region

--- IOSApplicationDaanVanMullem/ViewController.swift
+++ IOSApplicationDaanVanMullem/ViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 class ViewController: UIViewController  {
 
     @IBOutlet var searchField: UITextField!
+    @IBOutlet var regionPicker: UIPickerView!
     @IBOutlet var searchButton: UIButton!
+    
+    let regions = ["au","ca","dk","eu","no","ru","ch","gb","us"]
+    var selectedRegion = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        regionPicker.dataSource = self
+        regionPicker.delegate = self
+        regionPicker.selectRow(3, inComponent: 0, animated: true)
         searchField.textAlignment = .center
-        searchField.placeholder = "Please enter your Game titel"
+        searchField.placeholder = "Please enter your Game title"
         searchField.returnKeyType = .search
         searchField.autocapitalizationType = .words
         searchField.autocorrectionType = .no
@@ -32,6 +39,7 @@ class ViewController: UIViewController  {
         guard let storeVC = segue.destination as? StoresViewController else { return }
         let toSearch = searchField.text?.replacingOccurrences(of: " ", with: "-")
         storeVC.searchTerm = toSearch
+        storeVC.searchRegion = selectedRegion
     }
 }
 
@@ -44,3 +52,28 @@ extension ViewController: UITextFieldDelegate{
         return true
     }
 }
+
+extension ViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return regions.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return regions[row]
+    }
+    
+}
+
+extension ViewController: UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedRegion = regions[row]
+    }
+}
+
+
+
+
