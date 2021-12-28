6edf2dc0ee7bc1e254673c7458905df69894d19c
Daan <daan.vanmullem@student.hogent.be>
2021-12-28 14:37:06 +0000

    Configuration & segue

--- IOSApplicationDaanVanMullem/ViewController.swift
+++ IOSApplicationDaanVanMullem/ViewController.swift
@@ -8,14 +8,39 @@ import UIKit
 
 class ViewController: UIViewController  {
 
-    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet var searchField: UITextField!
+    @IBOutlet var searchButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchField.textAlignment = .center
+        searchField.placeholder = "Please enter your Game titel"
+        searchField.returnKeyType = .search
+        searchField.autocapitalizationType = .words
+        searchField.autocorrectionType = .no
+        searchField.delegate = self
     }
     
     @IBAction func search(_ sender: Any) {
-        performSegue(withIdentifier: "showStores", sender: self)
+        if let text = searchField.text{
+            print("\(text)")
+            performSegue(withIdentifier: "showStores", sender: self)
+        }
+}
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let storeVC = segue.destination as? StoresViewController else { return }
+        let toSearch = searchField.text?.replacingOccurrences(of: " ", with: "-")
+        storeVC.searchTerm = toSearch
     }
 }
 
+extension ViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = searchField.text{
+            print("\(text)")
+            self.performSegue(withIdentifier: "showStores", sender: self)
+        }
+        return true
+    }
+}
